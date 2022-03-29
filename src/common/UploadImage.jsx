4c877441d086c7d5ddc235443d63fd11454356da
Kiril Zyapkov <ziapkov@gmail.com>
2022-03-29 18:04:07 +0200

    Fixing overlay effect.

--- src/common/UploadImage.jsx
+++ src/common/UploadImage.jsx
@@ -18,7 +18,7 @@ function UploadImage({
     }
 
     return (
-        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay z-40'>
+        <div className='fixed flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay z-40 h-screen'>
 
             <form className='mt-3 flex flex-col' onSubmit={type === "banner" ? changeBanner : changePic}>
                 <input className='hidden' type='file' name='picture' id='file-input' required onChange={addPicture} />
@@ -27,6 +27,7 @@ function UploadImage({
 
             </form>
             <button className='bg-red-600 p-1 rounded' onClick={() => type === "banner" ? setToggleInputBanner(!toggleInputBanner) : setToggleInput(!toggleInput)}>Cancel</button>
+            
         </div>
     )
 }
